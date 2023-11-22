bc757dc8e5a228db51f4dfe945bc12abd1a0276c
S <unknown33572@gmail.com>
2023-11-22 13:54:57 +0900

    collection-framework

---
+++ src/oop/collection/ArrayListExample.java
@@ -0,0 +1,32 @@
+package oop.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>(); // List<String> list = new ArrayList<String>();
+
+    list.add("Java");
+    list.add("C++");
+    list.add(1, "Python"); // index 1에 Python 추가 그래서 C++는 index 2로 밀림
+    list.add("JavaScript");
+
+    int size = list.size();
+    System.out.println("list Size: " + size);
+
+    System.out.println();
+
+    for(String str : list) {
+      System.out.println(str);
+    }
+
+    System.out.println();
+
+    list.remove(2); // index 1인 C++ 삭제
+
+    for(int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+  }
+}
---
+++ src/oop/collection/VectorExample.java
@@ -0,0 +1,45 @@
+package oop.collection;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorExample {
+  public static void main(String[] args) {
+    List<Board> list = new Vector<>();
+
+    list.add(new Board("제목1", "내용1", "글쓴이1"));
+    list.add(new Board("제목2", "내용2", "글쓴이2"));
+    list.add(new Board("제목3", "내용3", "글쓴이3"));
+
+    list.remove(1);
+
+    for(int i = 0; i < list.size(); i++) {
+      Board board = list.get(i);
+      System.out.println(board.getTitle() + "\t" + board.getContent() + "\t" + board.getWriter());
+    } // index 1인 제목2, 내용2, 글쓴이2 삭제해서 제목3, 내용3, 글쓴이3이 index 1로 밀림
+  }
+}
+
+class Board {
+  private String title;
+  private String content;
+  private String writer;
+
+  public Board(String title, String content, String writer) {
+    this.title = title;
+    this.content = content;
+    this.writer = writer;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public String getWriter() {
+    return writer;
+  }
+}
